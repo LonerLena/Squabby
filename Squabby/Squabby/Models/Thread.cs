90358c2e8cc3b1ab42e887004c00f67f8c4403cb
Job Jobse <jobse@pm.me>
2020-06-09 17:24:56 +0200

    Added timestamp to Thread

--- Squabby/Squabby/Models/Thread.cs
+++ Squabby/Squabby/Models/Thread.cs
@@ -1,15 +1,20 @@
+using System;
 using System.Collections.Generic;
 
 namespace Squabby.Models
 {
     public class Thread
     {
+        public Thread() => creationDate = DateTime.Now; 
+        
         public int Id { get; set; }
 
         public string Title { get; set; }
         
         public string Content { get; set; }
 
+        public DateTime creationDate { get; set; }
+        
         public Board Board { get; set; }
 
         public List<Rating> Ratings { get; set; }
