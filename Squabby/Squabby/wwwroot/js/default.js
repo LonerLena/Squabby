b5905ebfd90479ad4d7d9412dd50118d2a6a611f
Job Jobse <jobse@pm.me>
2020-06-11 21:25:57 +0200

    Added javascript get function

--- Squabby/Squabby/wwwroot/js/default.js
+++ Squabby/Squabby/wwwroot/js/default.js
@@ -3,3 +3,17 @@ function postData(url) {
     xhr.open("POST", url, true);
     xhr.send();
 }
+
+function getData(url, callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status === 200) {
+        callback(null, xhr.response);
+      } else {
+        callback(status, xhr.response);
+      }
+    };
+    xhr.send();
+}
