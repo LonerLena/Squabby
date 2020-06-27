80bddee2dfe7a2c24f85f645f60d83d230bfbcd2
Job Jobse <jobse@pm.me>
2020-06-27 15:23:58 +0200

    Improved custom error page with "Go Back" button + added properties for custom button text + link

--- Squabby/Squabby/Models/ViewModels/Message.cs
+++ Squabby/Squabby/Models/ViewModels/Message.cs
@@ -1,14 +1,25 @@
 namespace Squabby.Models.ViewModels
 {
-    public class Message 
+    public class Message
     {
+        public Message()
+        {
+            ButtonText = "Go Back";
+            ButtonHref = "javascript:history.back()";
+        }
+
         public MessageType Type { get; set; }
         public string MessageString { get; set; }
         public string Description { get; set; }
+
+        public string ButtonText { get; set; }
+        public string ButtonHref { get; set; }
     }
 
-    public enum MessageType 
+    public enum MessageType
     {
-       Success, Warning, Error
+        Success,
+        Warning,
+        Error
     }
 }
